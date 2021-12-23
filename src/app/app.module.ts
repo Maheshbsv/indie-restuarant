1434fc567004da7b8950f32d3e975e1beb124eb7
Mahesh Babu S V <maheshbabu.sv@siemens.com>
2021-12-23 20:09:40 +0530

    adding services, header and footer components - missing styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,16 +9,22 @@ import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 
 import 'hammerjs';
-
+// Components
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
+// Services
+import { DishService } from './services/dish.service';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    DishdetailComponent
+    DishdetailComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
@@ -30,7 +36,9 @@ import { DishdetailComponent } from './dishdetail/dishdetail.component';
     MatCardModule,
     MatButtonModule,
   ],
-  providers: [],
+  providers: [
+    DishService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/shared/dish.ts
+++ src/app/shared/dish.ts
@@ -1,3 +1,5 @@
+import { Comment } from "./comment";
+
 export class Dish {
     id!: string;
     name!: string;
@@ -7,4 +9,5 @@ export class Dish {
     featured!: boolean;
     label!: string;
     price!: string;
+    comments!: Comment[];
 }
